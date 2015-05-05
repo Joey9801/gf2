ccbe89cbdd290158669555d0989f3391b99a54b8
Joe Roberts <joe@jwjr.co.uk>
2015-05-05 18:45:25 +0100

    Initial network implementation

---
+++ src/simulator/network.cc
@@ -0,0 +1,89 @@
+#include "network.h"
+
+Network::Network(void) :
+  BaseComponent("Network", 0, 0)
+{
+  _componentMap["And"] = &createComponent<AndGate>;
+  _componentMap["Nand"] = &createComponent<NandGate>;
+  _componentMap["Or"] = &createComponent<OrGate>;
+  _componentMap["Nor"] = &createComponent<NorGate>;
+  _componentMap["Xor"] = &createComponent<XorGate>;
+}
+
+Network::~Network() {}
+
+//returns componentId
+unsigned int Network::addComponent(std::string type) {
+  if(_componentMap.find(type) == _componentMap.end()) {
+    //TODO report an error
+    return 0;
+  }
+
+  unsigned int componentId = _components.size();
+  BaseComponent * c = _componentMap[type]();
+  for(unsigned int i=0; i<c->numOutputs(); i++)
+    c->setOutput(i, _nodesA.size()+i);
+  
+  _components.push_back(c);
+  _nodesA.resize(_nodesA.size() + c->numOutputs(), false);
+  _nodesB.resize(_nodesA.size());
+
+  return componentId;
+}
+
+//Returns the inputId
+unsigned int Network::addInput(void) {
+  unsigned int inputId = _inputs.size();
+  _inputs.push_back(0);
+  _inputMap.push_back(_nodesA.size());
+  _nodesA.push_back(false);
+  _nodesB.push_back(false);
+
+  return inputId;
+}
+
+unsigned int Network::addOutput(void) {
+  unsigned int outputId = _outputs.size();
+  _outputMap.push_back(0);
+  _outputs.push_back(0);
+  return outputId;
+}
+
+//Connect a member component to another member component
+void Network::connect(unsigned int idOut, unsigned int pinOut, unsigned int idIn, unsigned int pinIn) {
+  unsigned int node = _components[idOut]->getOutputNode(pinOut);
+  _components[idIn]->setInput(pinIn, node);
+  return;
+}
+
+//Connect a member component input to a network input
+void Network::connectInput(unsigned int inputId, unsigned int idIn, unsigned int pinIn) {
+  unsigned int node = _inputMap[inputId];
+  _components[idIn]->setInput(pinIn, node);
+  return;
+}
+
+//Connect a member component output to a network output
+void Network::connectOutput(unsigned int outputId, unsigned int idOut, unsigned int pinOut) {
+  unsigned int node = _components[idOut]->getOutputNode(pinOut);
+  _outputMap[outputId] = node;
+  return;
+}
+
+void Network::step(std::vector<bool>& a, std::vector<bool>& b) {
+
+  //Copy inputs into node list
+  for(unsigned int i=0; i<_inputs.size(); i++)
+    _nodesA[_inputMap[i]] = a[_inputs[i]];
+
+  for(auto c : _components)
+    c->step(_nodesA, _nodesB);
+
+  _nodesA.swap(_nodesB);
+
+  //Export the outputs
+  for(unsigned int i=0; i<_outputs.size(); i++)
+    b[_outputs[i]] = _nodesA[_outputMap[i]];
+
+  return;
+}
---
+++ src/simulator/network.h
@@ -0,0 +1,38 @@
+#ifndef NETWORK_H_
+#define NETWORK_H_
+
+#include <string>
+#include <vector>
+
+#include "basecomponent.h"
+#include "boolgates.h"
+
+class Network : public BaseComponent
+{
+  public:
+    Network();
+    ~Network();
+
+    void step(std::vector<bool>& a, std::vector<bool>& b);
+
+    unsigned int addComponent(std::string type);
+    unsigned int addInput(void);
+    unsigned int addOutput(void);
+
+    //Connecting two components
+    void connect(unsigned int idOut, unsigned int pinOut, unsigned int idIn, unsigned int pinIn);
+
+    //Connecting a component to an input
+    void connectInput(unsigned int inputId, unsigned int idIn, unsigned int pinIn);
+    void connectOutput(unsigned int outputId, unsigned int idOut, unsigned int pinOut);
+  private:
+    component_map _componentMap;
+
+    std::vector<unsigned int> _inputMap;      //Distinct from _inputs. Maps _input[i] to internal _node[j]
+    std::vector<unsigned int> _outputMap;      //Distinct from _outputs. Maps _output[i] to internal _node[j]
+    std::vector<BaseComponent*> _components;
+    std::vector<bool> _nodesA;
+    std::vector<bool> _nodesB;
+};
+
+#endif

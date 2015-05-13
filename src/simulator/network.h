7792ba05c28887b76652e85b85d422f9879602e2
Joe Roberts <joe@jwjr.co.uk>
2015-05-13 13:54:11 +0100

    Dummy components for network IO
    Network input/output is now represented by two dummy components with id's 0 and 1, and component names 'input' and 'output'.
    Network components can now have names + be renamed with same interface as BaseComponent pins
    Network::connect function replaced, any combination of the four arguments may be strings/uints, meaning component/pin names or component/pin id's as appropriate.
    
    closes #1

--- src/simulator/network.h
+++ src/simulator/network.h
@@ -8,6 +8,23 @@
 #include "boolgates.h"
 #include "flipflops.h"
 
+//Dummy IO component
+class DummyIO : public BaseComponent
+{
+  public:
+    DummyIO();
+    ~DummyIO();
+
+    void step(std::vector<bool>& a, std::vector<bool>& b);
+    void loadInputs(std::vector<bool>& source, std::vector<bool>& sink, std::vector<unsigned int> indicies);
+    void loadOutputs(std::vector<bool>& source, std::vector<bool>& sink, std::vector<unsigned int> indicies);
+
+    unsigned int addInput();
+    unsigned int addInput(std::string name);
+    unsigned int addOutput();
+    unsigned int addOutput(std::string name);
+};
+
 class Network : public BaseComponent
 {
   public:
@@ -17,25 +34,44 @@ class Network : public BaseComponent
     void step(std::vector<bool>& a, std::vector<bool>& b);
 
     unsigned int addComponent(std::string type);
+    unsigned int addComponent(std::string type, std::string name);
+
+    void renameComponent(std::string oldName, std::string newName);
+    void renameComponent(unsigned int id, std::string newName);
+    unsigned int findComponent(unsigned int componentId);
+    unsigned int findComponent(std::string componentName);
+
     unsigned int addInput(void);
     unsigned int addInput(std::string);
     unsigned int addOutput(void);
     unsigned int addOutput(std::string);
 
-    //Connecting two components
-    void connect(unsigned int idOut, unsigned int pinOut, unsigned int idIn, unsigned int pinIn);
+    // Connecting two components
+    // Annoying that templates must be defined in the header, but whatever
+    // The input 'pinIn' of componentIn is connected to the output 'pinOut' of componentOut
+    // Each argument may be a string or an unsigned integer. Strings for component/pin names,
+    // unsigned ints for component/pin id's
+    template<typename t1, typename t2, typename t3, typename t4>
+      void connect(t1 componentOut, t2 pinOut, t3 componentIn, t4 pinIn) {
+        unsigned int idOut = findComponent(componentOut);
+        unsigned int idIn = findComponent(componentIn);
 
-    //Connecting a component to an input
-    void connectInput(unsigned int inputId, unsigned int idIn, unsigned int pinIn);
-    void connectOutput(unsigned int outputId, unsigned int idOut, unsigned int pinOut);
+        unsigned int node = _components[idOut]->getOutputNode(pinOut);
+        _components[idIn]->setInput(pinIn, node);
+        return;
+      }
 
     unsigned int countComponents(void);
-  private:
-    component_map _componentMap;
 
-    std::vector<unsigned int> _inputMap;      //Distinct from _inputs. Maps _input[i] to internal _node[j]
-    std::vector<unsigned int> _outputMap;      //Distinct from _outputs. Maps _output[i] to internal _node[j]
+  private:
+    constructor_map _componentConstructor;
+
+    std::map<std::string, unsigned int> _componentNames;
     std::vector<BaseComponent*> _components;
+
+    DummyIO * _inputDummy;
+    DummyIO * _outputDummy;
+
     std::vector<bool> _nodesA;
     std::vector<bool> _nodesB;
 };

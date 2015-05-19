9c4a7405e74f34870f47884a62ecd5dd19d48cb2
Joe Roberts <joe@jwjr.co.uk>
2015-05-19 17:13:06 +0100

    Basic parser structure
    Layout of parser functions with skeletons+pseudocode in the bodies

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -4,6 +4,53 @@ namespace Parser {
   Definition * parseDefinition(std::string filepath) {
     Definition * def = new Definition();
 
+    //Opens a filestream
+
     return def;
   }
+
+  Definition * parseDict(std::ifstream& stream) {
+    Definition * def = new Definition(Dict);
+
+    //std::pair<std::string, Definition*> pair;
+    //while(not the end of the dict) {
+    //  pair = parsePair(stream);
+    //  def->pairs[pair.first] = pair.second;
+    //}
+    return def;
+  }
+
+  std::pair<std::string, Definition*> parsePair(std::ifstream& stream) {
+    std::pair<std::string, Definition*> pair;
+
+    skipWhitespace(stream);
+    pair.first = parseValue(stream);
+    skipWhitespace(stream);
+    if(stream.get() == '{')
+      pair.second = parseDict(stream);
+    else
+      pair.second = new Definition(parseValue(stream));
+
+
+    return pair;
+  }
+
+  std::string parseValue(std::ifstream& stream) {
+    //Reads a string in until one of the following characters are met:
+    //   } : ,
+    //If the first character is a quote, spaces are allowed.
+
+    std::string s;
+    s.reserve(100); //Bound to be enough for most things
+
+    //Does some magic
+
+    return s;
+  }
+
+  void skipWhitespace(std::ifstream& stream) {
+    while( isspace(stream.get()) );
+    stream.unget();
+  }
+
 }
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -2,11 +2,26 @@
 #define PARSER_H_
 
 #include <string>
+#include <fstream>
+#include <cctype>
 
 #include "../structures/definition.h"
 
 namespace Parser {
   Definition * parseDefinition(std::string filepath);
+
+  //Reads a dict from the first character after the opening brace
+  //until the closing brace
+  Definition * parseDict(std::ifstream& stream);
+
+  //Reads a pair. Returns the stream with the trailing comma/brace intact
+  std::pair<std::string, Definition*> parsePair(std::ifstream& stream);
+
+  //Reads an identifier
+  std::string parseValue(std::ifstream& stream);
+
+  //Does what it says on the tin
+  void skipWhitespace(std::ifstream& stream);
 }
 
 #endif

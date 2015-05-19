4e2f642f389fa035ae2b42cf60cfec7d718a62b7
Joe Roberts <joe@jwjr.co.uk>
2015-05-19 16:28:13 +0100

    Change std::vector<string> to std::vector<wxString>
    wxString type properly supports the unicode box-drawing characters needed for drawing the graph.

--- src/gui/outputplot.cc
+++ src/gui/outputplot.cc
@@ -39,41 +39,41 @@ void OutputPlot::plotData(string label, vector<bool> &data)
 */
 void OutputPlot::plotData(string label, vector<bool> &data)
 {
-  vector<string> plot(5, "");
+  vector<wxString> plot(5, "");
   for(unsigned int i = 0; i < data.size(); i++) {
     if (i == data.size()-1){
       if (data[i]==true){
-        plot[0].append("_\xe2\x94\x80\xe2\x94\x80");
-        plot[1].append("   ");
-        plot[2].append("   ");
+        plot[0].append(wxString::FromUTF8("\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80"));
+        plot[1].append(wxString::FromUTF8("   "));
+        plot[2].append(wxString::FromUTF8("   "));
       }else {
-        plot[0].append("   ");
-        plot[1].append("   ");
-        plot[2].append("\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80");
+        plot[0].append(wxString::FromUTF8("   "));
+        plot[1].append(wxString::FromUTF8("   "));
+        plot[2].append(wxString::FromUTF8("\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80"));
       }
     }else {
       if (data[i]==true and data[i+1]==true){ 
-        plot[0].append("\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80");
-        plot[1].append("   ");
-        plot[2].append("   ");
+        plot[0].append(wxString::FromUTF8("\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80"));
+        plot[1].append(wxString::FromUTF8("   "));
+        plot[2].append(wxString::FromUTF8("   "));
       }else if (data[i]==true and data[i+1]==false){
-        plot[0].append("\xe2\x94\x80\xe2\x94\x80");
-        plot[0].append("\xe2\x94\x90");
-        plot[1].append("  ");
-        plot[1].append("\xe2\x94\x82");
-        plot[2].append("  ");
-        plot[2].append("\xe2\x94\x94");
+        plot[0].append(wxString::FromUTF8("\xe2\x94\x80\xe2\x94\x80"));
+        plot[0].append(wxString::FromUTF8("\xe2\x94\x90"));
+        plot[1].append(wxString::FromUTF8("  "));
+        plot[1].append(wxString::FromUTF8("\xe2\x94\x82"));
+        plot[2].append(wxString::FromUTF8("  "));
+        plot[2].append(wxString::FromUTF8("\xe2\x94\x94"));
       }else if (data[i]==false and data[i+1]==true){
-        plot[0].append("  ");
-        plot[0].append("\xe2\x94\x8c");
-        plot[1].append("  ");
-        plot[1].append("\xe2\x94\x82");
-        plot[2].append("\xe2\x94\x80\xe2\x94\x80");
-        plot[2].append("\xe2\x94\x98");
+        plot[0].append(wxString::FromUTF8("  "));
+        plot[0].append(wxString::FromUTF8("\xe2\x94\x8c"));
+        plot[1].append(wxString::FromUTF8("  "));
+        plot[1].append(wxString::FromUTF8("\xe2\x94\x82"));
+        plot[2].append(wxString::FromUTF8("\xe2\x94\x80\xe2\x94\x80"));
+        plot[2].append(wxString::FromUTF8("\xe2\x94\x98"));
       }else if (data[i]==false and data[i+1]==false){
-        plot[0].append("   ");
-        plot[1].append("   ");
-        plot[2].append("\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80");
+        plot[0].append(wxString::FromUTF8("   "));
+        plot[1].append(wxString::FromUTF8("   "));
+        plot[2].append(wxString::FromUTF8("\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80"));
       }
     }
   }

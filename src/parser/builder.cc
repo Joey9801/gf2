bd90664ab3b75deaa098b94a1f65d82ea52c6c04
Joe Roberts <joe@jwjr.co.uk>
2015-05-19 15:18:37 +0100

    Initial builder implementation
    No testing, no semantic error checking. It does compile and not segfault though, so that's nice.

--- src/parser/builder.cc
+++ src/parser/builder.cc
@@ -1,5 +1,7 @@
 #include "builder.h"
 
+#include <iostream>
+
 namespace Builder {
   Network * build(std::string filepath) {
     Definition * def = Parser::parseDefinition(filepath);
@@ -28,6 +30,15 @@ namespace Builder {
     //Build each network in the includes and add to the includes map
     //for_each(include in def)
     //  includes[include_nickname] = build(include_path)
+
+    if( def->pairs.find("includes") != def->pairs.end() ) {
+      for(std::map<std::string, Definition*>::iterator it = def->pairs["includes"]->pairs.begin();
+          it != def->pairs["includes"]->pairs.end();
+          it++) {
+        includes[it->first] = build(it->second->value);
+      }
+    }
+
     return;
   }
 
@@ -35,6 +46,30 @@ namespace Builder {
     //Add all the named IO in def to net
     //for_each(element in def inputs/outputs)
     //  net->addInput/addOutput
+
+    if( def->pairs.find("inputs") != def->pairs.end() ) {
+      for(std::map<std::string, Definition*>::iterator it = def->pairs["inputs"]->pairs.begin();
+          it != def->pairs["inputs"]->pairs.end();
+          it++) {
+        net->addInput( it->first );
+      }
+    }
+    else {
+      //TODO add a warning
+    }
+
+    if( def->pairs.find("outputs") != def->pairs.end() ) {
+      //Add some outputs
+      for(std::map<std::string, Definition*>::iterator it = def->pairs["outputs"]->pairs.begin();
+          it != def->pairs["outputs"]->pairs.end();
+          it++) {
+        net->addInput( it->first );
+      }
+    }
+    else {
+      //TODO add a warning
+    }
+
     return;
   }
 
@@ -42,6 +77,20 @@ namespace Builder {
     //Add each component to the network
     //for_each(component_nickname in def)
     //  net->addComponent
+
+    if( def->pairs.find("components") == def->pairs.end() ) {
+      //TODO raise an error
+      return;
+    }
+    for(std::map<std::string, Definition*>::iterator it = def->pairs["components"]->pairs.begin();
+        it != def->pairs["components"]->pairs.end();
+        it++) {
+      std::string name = it->first;
+      std::string type = it->second->pairs["type"]->value;
+
+      net->addComponent(type, name);
+    }
+
     return;
   }
 
@@ -50,10 +99,44 @@ namespace Builder {
     //for_each(component in def)
     //  for_each(input in component_inputs)
     //    connect the input to the thing
+
+    if( def->pairs.find("outputs") != def->pairs.end() ) {
+      //Connect any outputs
+      for(std::map<std::string, Definition*>::iterator it = def->pairs["outputs"]->pairs.begin();
+          it != def->pairs["outputs"]->pairs.end();
+          it++) {
+        std::pair<std::string, std::string> dest = Helpers::separateDotted( it->second->value );
+        net->connect("outputs", it->first, dest.first, dest.second);
+      }
+    }
+
+    if( def->pairs.find("components") != def->pairs.end() ) {
+      for(std::map<std::string, Definition*>::iterator it1 = def->pairs["components"]->pairs.begin();
+          it1 != def->pairs["components"]->pairs.end();
+          it1++) {
+        //Iterating over each component
+        if( it1->second->pairs.find("inputs") != it1->second->pairs.end() ) {
+          //If the component has inputs
+          for(std::map<std::string, Definition*>::iterator it2 = it1->second->pairs["inputs"]->pairs.begin();
+              it2 != it1->second->pairs["inputs"]->pairs.end();
+              it2++) {
+            //Iterating over each input in a component
+            std::pair<std::string, std::string> source = Helpers::separateDotted( it2->second->value );
+            net->connect( source.first, source.second, it1->first, it2->first );
+          }
+        }
+      }
+    }
+
     return;
   }
 
   namespace Helpers {
-    std::pair<std::string, std::string> separateDotten(std::string in);
+    std::pair<std::string, std::string> separateDotted(std::string in) {
+      std::pair<std::string, std::string> out;
+      out.first = in.substr(0, in.find_first_of('.'));
+      out.second = in.substr(in.find_first_of('.')+1);
+      return out;
+    }
   }
 }

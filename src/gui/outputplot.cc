06992ce25ed55f785e1b1e907f5cf3fa6da18d67
Joe Roberts <joe@jwjr.co.uk>
2015-05-22 14:04:09 +0100

    refactored outputplot rendering

--- src/gui/outputplot.cc
+++ src/gui/outputplot.cc
@@ -1,5 +1,7 @@
 #include "outputplot.h"
 
+int wxglcanvas_attrib_list[5] = {WX_GL_RGBA, WX_GL_DOUBLEBUFFER, WX_GL_DEPTH_SIZE, 16, 0};
+
 OutputPlot::OutputPlot(wxWindow *parent, wxWindowID id)
   :   wxScrolledWindow(parent, id, wxDefaultPosition, wxDefaultSize, wxSUNKEN_BORDER|wxHSCROLL)
 {
@@ -24,8 +26,6 @@ void OutputPlot::AddPlotTrace(std::string label, std::vector<bool> &data)
   _plotcanvas->Render();
 }
 
-int wxglcanvas_attrib_list[5] = {WX_GL_RGBA, WX_GL_DOUBLEBUFFER, WX_GL_DEPTH_SIZE, 16, 0};
-
 MyGLCanvas::MyGLCanvas(wxWindow *parent, wxWindowID id) :
   wxGLCanvas(parent, id, wxglcanvas_attrib_list)
   // Constructor - initialises private variables
@@ -47,6 +47,8 @@ MyGLCanvas::MyGLCanvas(wxWindow *parent, wxWindowID id) :
   _monitortraces["Really long plot name that just goes on and on and on and on."] = data2;
 
   bitwidth = 30.0;
+  xzero = 100.0;
+  yzero  = 20.0;
 }
 
 void MyGLCanvas::Render()
@@ -58,13 +60,24 @@ void MyGLCanvas::Render()
   }
   glClear(GL_COLOR_BUFFER_BIT);
 
-  const float xzero = 100.0;
-  float yzero  = 20.0;
   rowheight = (GetSize().y-yzero) / _monitortraces.size();
-  float plotheight = 0.8*rowheight;
   SetMinSize(wxSize(_monitortraces.begin()->second.size()*bitwidth + 110, -1));
 
-  //draw x-axis
+  drawAxis();
+
+  typedef std::map<wxString, std::vector<bool>>::iterator it_type;
+  unsigned int i = 0;
+  for(it_type it=_monitortraces.begin(); it!=_monitortraces.end(); it++) {
+    drawPlot(i, it->first, it->second);
+    i++;
+  }
+
+  // We've been drawing to the back buffer, flush the graphics pipeline and swap the back buffer to the front
+  glFlush();
+  SwapBuffers();
+}
+
+void MyGLCanvas::drawAxis(void) {
   //draw axis line
   glColor3f(0.0, 0.0, 0.0);//axis colour
     glBegin(GL_LINE_STRIP);
@@ -84,37 +97,39 @@ void MyGLCanvas::Render()
       glVertex2f(x, yzero*0.3); 
     glEnd();
   }
+  return;
+}
 
-  //plot all the monitortraces
-  typedef std::map<wxString, std::vector<bool>>::iterator it_type;
-  for(it_type it=_monitortraces.begin(); it!=_monitortraces.end(); it++) {
-    //write labels, wrap line if longer than 9 chars
-    //if there is not enough vertical space, label is truncated
-    glColor3f(0.0, 0.0, 1.0);//label colour
-    for (unsigned int i = 0.0; it->first.Len()>9*i and rowheight*0.6>i*17.0; i++){
-      glRasterPos2f(10, yzero+rowheight*0.6-17.0*i);//label pos
-      wxString plotlabel = it->first.Mid(i*9, 9);//truncate label to fit
-      //write label
-      for (unsigned int j = 0; j < plotlabel.Len(); j++) glutBitmapCharacter(GLUT_BITMAP_9_BY_15, plotlabel[j]);
-    }
+void MyGLCanvas::drawPlot(
+    unsigned int num,
+    const wxString& label,
+    const std::vector<bool>& data)
+{
+  float base = yzero + (num * rowheight);
 
-    //draw actual plot traces
-    glColor3f(0.0, 1.0, 0.0);//plot colour
-    glBegin(GL_LINE_STRIP);
-    for (unsigned int i = 0; i<it->second.size(); i++) {
-      float y;
-      if (it->second[i]) y = plotheight;
-      else y = 0.0;
-      glVertex2f(xzero + i*bitwidth, yzero+y); 
-      glVertex2f(xzero + (i+1)*bitwidth, yzero+y);
-    }
-    glEnd();
-    yzero += rowheight;
-  } 
+  //write labels, wrap line if longer than 9 chars
+  //if there is not enough vertical space, label is truncated
+  glColor3f(0.0, 0.0, 1.0);
+  for (unsigned int i = 0.0; label.Len()>9*i and rowheight*0.6>i*17.0; i++) {
+    glRasterPos2f(10, base + rowheight*0.6-17.0*i);//label pos
+    wxString plotlabel = label.Mid(i*9, 9);//truncate label to fit
+    //write label
+    for (unsigned int j = 0; j < plotlabel.Len(); j++)
+      glutBitmapCharacter(GLUT_BITMAP_9_BY_15, plotlabel[j]);
+  }
 
-  // We've been drawing to the back buffer, flush the graphics pipeline and swap the back buffer to the front
-  glFlush();
-  SwapBuffers();
+  //draw actual plot traces
+  glColor3f(0.0, 1.0, 0.0);//plot colour
+  glBegin(GL_LINE_STRIP);
+  for (unsigned int i = 0; i<data.size(); i++) {
+
+    float y = data[i] ? (rowheight*0.8) : 0;
+
+    glVertex2f(xzero + i*bitwidth, base + y);
+    glVertex2f(xzero + (i+1)*bitwidth, base + y);
+  }
+  glEnd();
+  return;
 }
 
 void MyGLCanvas::InitGL()
@@ -134,27 +149,27 @@ void MyGLCanvas::InitGL()
   glLoadIdentity();
 }
 
-void MyGLCanvas::OnPaint(wxPaintEvent& event)
-  // Event handler for when the canvas is exposed
-{
-  wxPaintDC dc(this); // required for correct refreshing under MS windows
+// Event handler for when the canvas is exposed
+void MyGLCanvas::OnPaint(wxPaintEvent& event) {
+  // required for correct refreshing under MS windows
+  wxPaintDC dc(this);
   Render();
 }
 
-void MyGLCanvas::OnSize(wxSizeEvent& event)
-  // Event handler for when the canvas is resized
-{
-  init = false;; // this will force the viewport and projection matrices to be reconfigured on the next paint
+// Event handler for when the canvas is resized
+void MyGLCanvas::OnSize(wxSizeEvent& event) {
+  // this will force the viewport and projection matrices to be reconfigured on the next paint
+  init = false;
 }
 
-void MyGLCanvas::OnMousewheel(wxMouseEvent& event)
-{
-  if (event.GetWheelRotation()>0.0){
+void MyGLCanvas::OnMousewheel(wxMouseEvent& event) {
+  if (event.GetWheelRotation()>0.0) {
     bitwidth += 5;
-  }else if (event.GetWheelRotation()<0.0){
+  }
+  else if (event.GetWheelRotation()<0.0) {
     bitwidth -= 5.0;
   }
-  if (bitwidth<5.0){
+  if (bitwidth<5.0) {
     bitwidth = 5.0;
   }
   init = false;

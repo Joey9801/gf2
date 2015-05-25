9bff103bed1ffd8301dff77467247ecc7ef0d6de
Duncan Barber <dab67@cam.ac.uk>
2015-05-25 18:14:22 +0100

    Created the charcheck header, which provides functions which allow a character type to be checked, and a test unit for it

---
+++ src/parser/charcheck.cc
@@ -0,0 +1,58 @@
+#include "charcheck.h"
+
+#include <cctype>
+
+/// Check whether the given character is a "letter"
+bool isLetterChar(char ch) {
+  return isalpha(ch);
+}
+
+/// Check whether the given character is a "digit"
+bool isDigitChar(char ch) {
+  return isdigit(ch);
+}
+
+/// Check whether the given character is a "character"
+bool isCharacterChar(char ch) {
+  return isLetterChar(ch) || isDigitChar(ch) || ch == '_';
+}
+
+/// Check whether the given character is a "whitespace" character
+bool isWhitespaceChar(char ch) {
+  return isspace(ch);
+}
+
+/// Check whether the given character is a "punctuation" character
+bool isPunctuationChar(char ch) {
+  return ispunct(ch);
+}
+
+/// Check whether the given character is a valid "string" character
+bool isStringChar(char ch) {
+  return isCharacterChar(ch) || isWhitespaceChar(ch) || isPunctuationChar(ch);
+}
+
+/// Check whether the given character is a valid "comment" character
+bool isCommentChar(char ch) {
+  return isCharacterChar(ch) || isWhitespaceChar(ch) || isPunctuationChar(ch);
+}
+
+/// Check whether the given character is a valid "dict" opening delimiter
+bool isOpeningDictDelim(char ch) {
+  return ch == '{';
+}
+
+/// Check whether the given character is a valid "dict" closing delimiter
+bool isClosingDictDelim(char ch) {
+  return ch == '}';
+}
+
+/// Check whether the given character is a valid "dict" separator
+bool isDictSeparator(char ch) {
+  return ch == ',';
+}
+
+/// Check whether the given character is a valid "pair" separator
+bool isPairSeparator(char ch) {
+  return ch == ':';
+}
---
+++ src/parser/charcheck.h
@@ -0,0 +1,31 @@
+/*! \file charcheck.h
+          Contains the declarations of functions to check characters
+          against the EBNF definitions.
+*/
+
+#ifndef CHARCHECK_H_
+#define CHARCHECK_H_
+
+bool isLetterChar(char ch);
+
+bool isDigitChar(char ch);
+
+bool isCharacterChar(char ch);
+
+bool isWhitespaceChar(char ch);
+
+bool isPunctuationChar(char ch);
+
+bool isStringChar(char ch);
+
+bool isCommentChar(char ch);
+
+bool isOpeningDictDelim(char ch);
+
+bool isClosingDictDelim(char ch);
+
+bool isDictSeparator(char ch);
+
+bool isPairSeparator(char ch);
+
+#endif // CHARCHECK_H_

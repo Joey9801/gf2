0bbfcb57fc6e7bf40dd25da6a276708319ee8db6
Duncan Barber <dab67@cam.ac.uk>
2015-05-27 23:26:30 +0100

    Made arguments const in all charcheck module functions

--- src/parser/charcheck.cc
+++ src/parser/charcheck.cc
@@ -3,62 +3,62 @@
 #include <cctype>
 
 /// Check whether the given character is a "letter"
-bool isLetterChar(char ch) {
+bool isLetterChar(const char ch) {
   return isalpha(ch);
 }
 
 /// Check whether the given character is a "digit"
-bool isDigitChar(char ch) {
+bool isDigitChar(const char ch) {
   return isdigit(ch);
 }
 
 /// Check whether the given character is a "character"
-bool isCharacterChar(char ch) {
+bool isCharacterChar(const char ch) {
   return isLetterChar(ch) || isDigitChar(ch) || ch == '_';
 }
 
 /// Check whether the given character is a "whitespace" character
-bool isWhitespaceChar(char ch) {
+bool isWhitespaceChar(const char ch) {
   return isspace(ch);
 }
 
 /// Check whether the given character is a "punctuation" character
-bool isPunctuationChar(char ch) {
+bool isPunctuationChar(const char ch) {
   return ispunct(ch);
 }
 
 /// Check whether the given character is a valid "string" character
-bool isStringChar(char ch) {
+bool isStringChar(const char ch) {
   return isCharacterChar(ch) || isWhitespaceChar(ch) || isPunctuationChar(ch);
 }
 
 /// Check whether the given character is a valid "comment" character
-bool isCommentChar(char ch) {
+bool isCommentChar(const char ch) {
   return isCharacterChar(ch) || isWhitespaceChar(ch) || isPunctuationChar(ch);
 }
 
 /// Check whether the given character is a valid "dict" opening delimiter
-bool isOpeningDictDelim(char ch) {
+bool isOpeningDictDelim(const char ch) {
   return ch == '{';
 }
 
 /// Check whether the given character is a valid "dict" closing delimiter
-bool isClosingDictDelim(char ch) {
+bool isClosingDictDelim(const char ch) {
   return ch == '}';
 }
 
 /// Check whether the given character is a valid "dict" separator
-bool isDictSeparator(char ch) {
+bool isDictSeparator(const char ch) {
   return ch == ',';
 }
 
 /// Check whether the given character is a valid "pair" separator
-bool isPairSeparator(char ch) {
+bool isPairSeparator(const char ch) {
   return ch == ':';
 }
 
 /// Check whether the given character is any valid character in the language
-bool isValidCharacter(char ch) {
+bool isValidCharacter(const char ch) {
   return  isStringChar(ch)        ||
           isCommentChar(ch)       ||
           isOpeningDictDelim(ch)  ||
--- src/parser/charcheck.h
+++ src/parser/charcheck.h
@@ -6,28 +6,28 @@
 #ifndef CHARCHECK_H_
 #define CHARCHECK_H_
 
-bool isLetterChar(char ch);
+bool isLetterChar(const char ch);
 
-bool isDigitChar(char ch);
+bool isDigitChar(const char ch);
 
-bool isCharacterChar(char ch);
+bool isCharacterChar(const char ch);
 
-bool isWhitespaceChar(char ch);
+bool isWhitespaceChar(const char ch);
 
-bool isPunctuationChar(char ch);
+bool isPunctuationChar(const char ch);
 
-bool isStringChar(char ch);
+bool isStringChar(const char ch);
 
-bool isCommentChar(char ch);
+bool isCommentChar(const char ch);
 
-bool isOpeningDictDelim(char ch);
+bool isOpeningDictDelim(const char ch);
 
-bool isClosingDictDelim(char ch);
+bool isClosingDictDelim(const char ch);
 
-bool isDictSeparator(char ch);
+bool isDictSeparator(const char ch);
 
-bool isPairSeparator(char ch);
+bool isPairSeparator(const char ch);
 
-bool isValidCharacter(char ch);
+bool isValidCharacter(const char ch);
 
 #endif // CHARCHECK_H_

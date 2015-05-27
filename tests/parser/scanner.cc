bcc7af9d72f08634f77ec33fd82140f1463a05f1
Duncan Barber <dab67@cam.ac.uk>
2015-05-27 23:17:10 +0100

    Expanded Scanner test unit to test all characters input in all states and to test all characters as first and last characters in a file which contains more than one character

--- tests/parser/scanner.cc
+++ tests/parser/scanner.cc
@@ -51,22 +51,390 @@ SCENARIO("Create files containing each character and run the scanner on these") 
 
   std::vector<Lexeme> lexemes;
   std::vector<ParserError> errors;
-  // Check that the files return an error and no lexemes if they should, and
-  // that the files return a lexeme and no errors if they should
+  // Check that the files return what would be expected
   for (int character = 0; character < 256; ++character) {
     lexemes.clear();
     errors.clear();
     scanner.scan(getCharacterFilename(character, folderName), errors, lexemes);
-    bool shouldBeErrorless = true;
-    if ((!isValidCharacter(character)) || (char(character) == '"')) {
-      shouldBeErrorless = false;
+    if (isValidCharacter(character)) {
+      if (char(character) == '"') {
+        // The string delimiter starts a string lexeme, which will be
+        // unterminated at the end of the file giving 1 error and 0 lexemes
+        REQUIRE(errors.size() == 1);
+        CHECK(errors[0].getErrorDescription() ==
+            "SCANNER ERROR: String unterminated by end of file");
+        CHECK(lexemes.empty());
+      } else {
+        // All other valid characters should give 0 errors and 1 lexeme
+        CHECK(errors.empty());
+        CHECK(lexemes.size() == 1);
+      }
+    } else {
+      // All invalid characters should give 1 error and 0 lexemes
+      REQUIRE(errors.size() == 1);
+      CHECK(errors[0].getErrorDescription() ==
+          "SCANNER ERROR: Invalid character in file - aborted scanning");
+      CHECK(lexemes.empty());
     }
-    if (shouldBeErrorless) {
-      CHECK(errors.empty());
-      CHECK(!lexemes.empty());
+  }
+}
+
+/// Create start character files for all 256 characters and test that they get
+/// scanned correctly
+SCENARIO("Create files starting with each character and run the scanner on these") {
+  const std::string folderName = "charactersatstart";
+  // Make sure the files are all successfully created
+  REQUIRE(createOneFilePerCharacter("", " at start", folderName) == 256);
+
+  Scanner scanner;
+
+  std::vector<Lexeme> lexemes;
+  std::vector<ParserError> errors;
+  // Check that the files return what would be expected
+  for (int character = 0; character < 256; ++character) {
+    lexemes.clear();
+    errors.clear();
+    scanner.scan(getCharacterFilename(character, folderName), errors, lexemes);
+    if (isValidCharacter(character)) {
+      if (char(character) == '"') {
+        // The string delimiter starts a string lexeme, which will be
+        // unterminated at the end of the file giving 1 error and 0 lexemes
+        REQUIRE(errors.size() == 1);
+        CHECK(errors[0].getErrorDescription() ==
+            "SCANNER ERROR: String unterminated by end of file");
+        CHECK(lexemes.empty());
+      } else {
+        CHECK(errors.empty());
+        if (isWhitespaceChar(character)) {
+          // Whitespace characters should combine with the space at the start of
+          // " at start" and hence give 0 errors and 4 lexemes
+          CHECK(lexemes.size() == 4);
+        } else {
+          // All other valid characters should not combine with the space at the
+          // start of " at start" and hence give 0 errors and 5 lexemes
+          CHECK(lexemes.size() == 5);
+        }
+      }
+    } else {
+      // All invalid characters should give 1 error and 0 lexemes
+      REQUIRE(errors.size() == 1);
+      CHECK(errors[0].getErrorDescription() ==
+          "SCANNER ERROR: Invalid character in file - aborted scanning");
+      CHECK(lexemes.empty());
     }
-    else {
-      CHECK(!errors.empty());
+  }
+}
+
+/// Create files for all 256 characters in the IDLE state and test that they
+/// get scanned correctly. Also serves as a test of all the characters being
+/// the last in the file
+SCENARIO("Create files with each character input in the IDLE state and run the scanner on these") {
+  const std::string folderName = "charactersallstates/idle";
+  // Make sure the files are all successfully created
+  REQUIRE(createOneFilePerCharacter("character:", "", folderName) == 256);
+
+  Scanner scanner;
+
+  std::vector<Lexeme> lexemes;
+  std::vector<ParserError> errors;
+  // Check that the files return what would be expected
+  for (int character = 0; character < 256; ++character) {
+    lexemes.clear();
+    errors.clear();
+    scanner.scan(getCharacterFilename(character, folderName), errors, lexemes);
+    if (isValidCharacter(character)) {
+      if (char(character) == '"') {
+        // The string delimiter starts a string lexeme, which will be
+        // unterminated at the end of the file giving 1 error and 2 lexemes
+        REQUIRE(errors.size() == 1);
+        CHECK(errors[0].getErrorDescription() ==
+            "SCANNER ERROR: String unterminated by end of file");
+        CHECK(lexemes.size() == 2);
+      } else {
+        // All other characters should give 0 errors and 3 lexemes
+        CHECK(errors.empty());
+        CHECK(lexemes.size() == 3);
+      }
+    } else {
+      // All invalid characters should give 1 error and 2 lexemes before this
+      // error is produced and scanning aborted
+      REQUIRE(errors.size() == 1);
+      CHECK(errors[0].getErrorDescription() ==
+          "SCANNER ERROR: Invalid character in file - aborted scanning");
+      CHECK(lexemes.size() == 2);
+    }
+  }
+}
+
+/// Create files for all 256 characters in the IDENTIFIERPROCESSING state and
+/// test that they get scanned correctly
+SCENARIO("Create files with each character input in the IDENTIFIERPROCESSING state and run the scanner on these") {
+  const std::string folderName = "charactersallstates/identifierprocessing";
+  // Make sure the files are all successfully created
+  REQUIRE(createOneFilePerCharacter("identStart", "End", folderName) == 256);
+
+  Scanner scanner;
+
+  std::vector<Lexeme> lexemes;
+  std::vector<ParserError> errors;
+  // Check that the files return what would be expected
+  for (int character = 0; character < 256; ++character) {
+    lexemes.clear();
+    errors.clear();
+    scanner.scan(getCharacterFilename(character, folderName), errors, lexemes);
+    if (isValidCharacter(character)) {
+      if (char(character) == '"') {
+        // The string delimiter starts a string lexeme, which will be
+        // unterminated at the end of the file giving 1 error and 1 lexemes
+        REQUIRE(errors.size() == 1);
+        CHECK(errors[0].getErrorDescription() ==
+            "SCANNER ERROR: String unterminated by end of file");
+        CHECK(lexemes.size() == 1);
+      } else {
+        CHECK(errors.empty());
+        if (isCharacterChar(character)) {
+          // Valid identifier characters should combine with the pre- and
+          // post- strings and hence give 0 errors and 1 lexeme
+          CHECK(lexemes.size() == 1);
+        } else {
+          // All other valid characters should not combine with the pre- and
+          // post- strings and hence give 0 errors and 3 lexemes
+          CHECK(lexemes.size() == 3);
+        }
+      }
+    } else {
+      // All invalid characters should give 1 error and 0 lexemes
+      REQUIRE(errors.size() == 1);
+      CHECK(errors[0].getErrorDescription() ==
+          "SCANNER ERROR: Invalid character in file - aborted scanning");
+      CHECK(lexemes.empty());
+    }
+  }
+}
+
+/// Create files for all 256 characters in the STRINGPROCESSING state and
+/// test that they get scanned correctly
+SCENARIO("Create files with each character input in the STRINGPROCESSING state and run the scanner on these") {
+  const std::string folderName = "charactersallstates/stringprocessing";
+  // Make sure the files are all successfully created
+  REQUIRE(createOneFilePerCharacter("\"string:", "\"", folderName) == 256);
+
+  Scanner scanner;
+
+  std::vector<Lexeme> lexemes;
+  std::vector<ParserError> errors;
+  // Check that the files return what would be expected
+  for (int character = 0; character < 256; ++character) {
+    lexemes.clear();
+    errors.clear();
+    scanner.scan(getCharacterFilename(character, folderName), errors, lexemes);
+    if (isValidCharacter(character)) {
+      if (isStringChar(character)) {
+        if (char(character) == '"') {
+          // The string delimiter closes the string lexeme, and then the
+          // post-string string delimiter starts another string lexeme, which
+          // will be unterminated at the end of the file giving 1 error and 1
+          // lexeme
+          REQUIRE(errors.size() == 1);
+          CHECK(errors[0].getErrorDescription() ==
+              "SCANNER ERROR: String unterminated by end of file");
+        } else {
+          // All other valid string characters will become part of the string
+          // lexeme, and hence should give 0 errors and 1 lexeme
+          CHECK(errors.empty());
+        }
+        CHECK(lexemes.size() == 1);
+      } else {
+        // Invalid string characters should give 1 error and 0 lexemes
+        REQUIRE(errors.size() == 1);
+        CHECK(errors[0].getErrorDescription() ==
+            "SCANNER ERROR: Invalid character in string - aborted scanning");
+        CHECK(lexemes.empty());
+      }
+    } else {
+      // All invalid characters should give 1 error and 0 lexemes
+      REQUIRE(errors.size() == 1);
+      CHECK(errors[0].getErrorDescription() ==
+          "SCANNER ERROR: Invalid character in file - aborted scanning");
+      CHECK(lexemes.empty());
+    }
+  }
+}
+
+/// Create files for all 256 characters in the COMMENTPROCESSING state and
+/// test that they get scanned correctly
+SCENARIO("Create files with each character input in the COMMENTPROCESSING state and run the scanner on these") {
+  const std::string folderName = "charactersallstates/commentprocessing";
+  // Make sure the files are all successfully created
+  REQUIRE(createOneFilePerCharacter("/*comment:", "", folderName) == 256);
+
+  Scanner scanner;
+
+  std::vector<Lexeme> lexemes;
+  std::vector<ParserError> errors;
+  // Check that the files return what would be expected
+  for (int character = 0; character < 256; ++character) {
+    lexemes.clear();
+    errors.clear();
+    scanner.scan(getCharacterFilename(character, folderName), errors, lexemes);
+    REQUIRE(errors.size() == 1);
+    if (isValidCharacter(character)) {
+      if (isCommentChar(character)) {
+        // All valid comment characters will become part of the unterminated
+        // comment and hence should give 1 error and 0 lexemes
+        CHECK(errors[0].getErrorDescription() ==
+            "SCANNER ERROR: Comment unterminated by end of file");
+      } else {
+        // All invalid comment characters should give 1 error and 0 lexemes
+        CHECK(errors[0].getErrorDescription() ==
+            "SCANNER ERROR: Invalid character in comment - aborted scanning");
+      }
+    } else {
+      // All invalid characters should give 1 error and 0 lexemes
+      REQUIRE(errors.size() == 1);
+      CHECK(errors[0].getErrorDescription() ==
+          "SCANNER ERROR: Invalid character in file - aborted scanning");
+    }
+    CHECK(lexemes.empty());
+  }
+}
+
+/// Create files for all 256 characters in the WHITESPACEPROCESSING state and
+/// test that they get scanned correctly
+SCENARIO("Create files with each character input in the WHITESPACEPROCESSING state and run the scanner on these") {
+  const std::string folderName = "charactersallstates/whitespaceprocessing";
+  // Make sure the files are all successfully created
+  REQUIRE(createOneFilePerCharacter("   ", "  ", folderName) == 256);
+
+  Scanner scanner;
+
+  std::vector<Lexeme> lexemes;
+  std::vector<ParserError> errors;
+  // Check that the files return what would be expected
+  for (int character = 0; character < 256; ++character) {
+    lexemes.clear();
+    errors.clear();
+    scanner.scan(getCharacterFilename(character, folderName), errors, lexemes);
+    if (isValidCharacter(character)) {
+      if (char(character) == '"') {
+        // The string delimiter starts a string lexeme, which will be
+        // unterminated at the end of the file giving 1 error and 1 lexemes
+        REQUIRE(errors.size() == 1);
+        CHECK(errors[0].getErrorDescription() ==
+            "SCANNER ERROR: String unterminated by end of file");
+        CHECK(lexemes.size() == 1);
+      } else {
+        CHECK(errors.empty());
+        if (isWhitespaceChar(character)) {
+          // Valid whitespace characters should combine with the pre- and
+          // post- strings and hence give 0 errors and 1 lexeme
+          CHECK(lexemes.size() == 1);
+        } else {
+          // All other valid characters should not combine with the pre- and
+          // post- strings and hence give 0 errors and 3 lexemes
+          CHECK(lexemes.size() == 3);
+        }
+      }
+    } else {
+      // All invalid characters should give 1 error and 0 lexemes
+      REQUIRE(errors.size() == 1);
+      CHECK(errors[0].getErrorDescription() ==
+          "SCANNER ERROR: Invalid character in file - aborted scanning");
+      CHECK(lexemes.empty());
+    }
+  }
+}
+
+/// Create files for all 256 characters in the POTENTIALBEGINCOMMENT state and
+/// test that they get scanned correctly
+SCENARIO("Create files with each character input in the POTENTIALBEGINCOMMENT state and run the scanner on these") {
+  const std::string folderName = "charactersallstates/potentialbegincomment";
+  // Make sure the files are all successfully created
+  REQUIRE(createOneFilePerCharacter("/", "", folderName) == 256);
+
+  Scanner scanner;
+
+  std::vector<Lexeme> lexemes;
+  std::vector<ParserError> errors;
+  // Check that the files return what would be expected
+  for (int character = 0; character < 256; ++character) {
+    lexemes.clear();
+    errors.clear();
+    scanner.scan(getCharacterFilename(character, folderName), errors, lexemes);
+    if (isValidCharacter(character)) {
+      if (char(character) == '*') {
+        // The * character starts a comment lexeme, which will be unterminated
+        // at the end of the file giving 1 error and 0 lexemes
+        REQUIRE(errors.size() == 1);
+        CHECK(errors[0].getErrorDescription() ==
+            "SCANNER ERROR: Comment unterminated by end of file");
+        CHECK(lexemes.empty());
+      } else {
+        if (char(character) == '"') {
+          // The string delimiter starts a string lexeme, which will be
+          // unterminated at the end of the file giving 1 error and 1 lexemes
+          REQUIRE(errors.size() == 1);
+          CHECK(errors[0].getErrorDescription() ==
+              "SCANNER ERROR: String unterminated by end of file");
+          CHECK(lexemes.size() == 1);
+        } else {
+          // All other valid characters should give 0 errors and 2 lexemes
+          CHECK(errors.empty());
+          CHECK(lexemes.size() == 2);
+        }
+      }
+    } else {
+      // All invalid characters should give 1 error and 0 lexemes
+      REQUIRE(errors.size() == 1);
+      CHECK(errors[0].getErrorDescription() ==
+          "SCANNER ERROR: Invalid character in file - aborted scanning");
+      CHECK(lexemes.empty());
+    }
+  }
+}
+
+/// Create files for all 256 characters in the POTENTIALENDCOMMENT state and
+/// test that they get scanned correctly
+SCENARIO("Create files with each character input in the POTENTIALENDCOMMENT state and run the scanner on these") {
+  const std::string folderName = "charactersallstates/potentialendcomment";
+  // Make sure the files are all successfully created
+  REQUIRE(createOneFilePerCharacter("/*comment*", "", folderName) == 256);
+
+  Scanner scanner;
+
+  std::vector<Lexeme> lexemes;
+  std::vector<ParserError> errors;
+  // Check that the files return what would be expected
+  for (int character = 0; character < 256; ++character) {
+    lexemes.clear();
+    errors.clear();
+    scanner.scan(getCharacterFilename(character, folderName), errors, lexemes);
+    if (isValidCharacter(character)) {
+      if (char(character) == '/') {
+        // The / character closes a comment lexeme, giving 0 errors and 1
+        // lexeme
+        CHECK(errors.empty());
+        CHECK(lexemes.size() == 1);
+      } else {
+        REQUIRE(errors.size() == 1);
+        if (isCommentChar(character)) {
+          // All valid comment characters will become part of the unterminated
+          // comment and hence should give 1 error and 0 lexemes
+          CHECK(errors[0].getErrorDescription() ==
+              "SCANNER ERROR: Comment unterminated by end of file");
+        } else {
+          // All other characters are invalid comment characters and hence give
+          // 1 error and 0 lexemes
+          CHECK(errors[0].getErrorDescription() ==
+              "SCANNER ERROR: Invalid character in comment - aborted scanning");
+        }
+        CHECK(lexemes.empty());
+      }
+    } else {
+      // All invalid characters should give 1 error and 0 lexemes
+      REQUIRE(errors.size() == 1);
+      CHECK(errors[0].getErrorDescription() ==
+          "SCANNER ERROR: Invalid character in file - aborted scanning");
       CHECK(lexemes.empty());
     }
   }

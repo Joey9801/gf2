735412531ecd50903c09c30ffcb98dc177b959a3
Duncan Barber <dab67@cam.ac.uk>
2015-05-29 01:18:32 +0100

    Clear parser header and source files ready for development of Parser class

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,30 +1,19 @@
+/*! \file parser.h
+          Contains the definition of the Parser class.
+*/
+
 #ifndef PARSER_H_
 #define PARSER_H_
 
 #include <string>
-#include <vector>
-#include <fstream>
-#include <istream>
-#include <iostream>
-#include <cctype>
 
 #include "../structures/definition.h"
 
-namespace Parser {
-  const std::vector<char> delimiters = {'{', '}', ':', ','};
-  Definition * parseDefinition(std::string filepath);
+/// Circuit definition file parser class
+/*! Uses a lexor to obtain all the tokens from a source file, and then parses
+    these to produce a circuit definition. */
+class Parser {
 
-  //Reads a dict from the first character after the opening brace
-  //until the closing brace
-  Definition * parseDict(std::istream& stream);
+};
 
-  //Reads a pair. Returns the stream with the trailing comma/brace intact
-  std::pair<std::string, Definition*> parsePair(std::istream& stream);
-
-  //Reads an identifier
-  std::string parseValue(std::istream& stream);
-
-  bool isdelim(char c);
-}
-
-#endif
+#endif // PARSER_H_

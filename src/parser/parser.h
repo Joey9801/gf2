f95db02d6d030a306f3b993ec08a8dc48b026b98
Duncan Barber <dab67@cam.ac.uk>
2015-05-29 09:10:03 +0100

    Made a start on Parser class implementation

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,14 +6,34 @@
 #define PARSER_H_
 
 #include <string>
+#include <vector>
 
+#include "lexor.h"
 #include "../structures/definition.h"
 
 /// Circuit definition file parser class
 /*! Uses a lexor to obtain all the tokens from a source file, and then parses
     these to produce a circuit definition. */
 class Parser {
+ public:
+  Parser();
 
+  Definition* parse(    const std::string filename,
+                        std::vector<ParserError>& errors);
+
+  Definition* parseDict(std::vector<Token>& tokens,
+                        std::vector<Token>::iterator& currentToken,
+                        std::vector<ParserError>& errors);
+
+  std::pair<std::string, Definition*> parsePair(
+                        std::vector<Token>& tokens,
+                        std::vector<Token>::iterator& currentToken,
+                        std::vector<ParserError>& errors);
+
+ private:
+  std::string addParserErrorPrefix(const std::string errorMessage);
+
+  Lexor lexor;  ///<  The parser's internal lexor object
 };
 
 #endif // PARSER_H_

d20d226bd6a82e372b60a85c91056a25b8d078cb
Daniel <djp73@cam.ac.uk>
2015-06-01 15:08:31 +0100

    Small translation errors fixed

--- getwordlist.sh
+++ getwordlist.sh
@@ -2,4 +2,4 @@
 # it searches for strings marked with _() in all .cpp files in directory ./src
 # the generated file is called 'logicsim.pot' and is generated in ./po
 CPP_FILE_LIST=`find ./src -name '*.cc' -print`
-xgettext -d app_name -s --keyword=_ -p ./po -o logicsim.pot $CPP_FILE_LIST
+xgettext -d app_name -s --keyword=_ -p ./i18n -o logicsim.pot $CPP_FILE_LIST

9f361afc5835e7ba2e39927d85e6af7c96ebb3f9
Duncan Barber <dab67@cam.ac.uk>
2015-06-01 17:20:03 +0100

    Started developing a test module for the Parser class

--- tests/parser/parser.cc
+++ tests/parser/parser.cc
@@ -1,105 +1,126 @@
 #include <catch.h>
 
-#include <istream>
-#include <utility>
-
 #include "../../src/parser/parser.h"
 #include "../../src/structures/definition.h"
 
-SCENARIO("ParseValue works independantly") {
-  WHEN("ParseValue is called with a : delimiter") {
-    std::istringstream input("   foo_ident  :  ");
-    std::string value = Parser::parseValue(input);
-    THEN("The value is read correctly") {
-      REQUIRE( value.compare("foo_ident") == 0 );
-    }
-    AND_THEN("The next character is the : delimiter") {
-      REQUIRE( (char)input.get() == ':' );
-    }
-  }
-  WHEN("ParseValue is called with a , delimiter") {
-    std::istringstream input("   foo_ident  ,  ");
-    std::string value = Parser::parseValue(input);
-    THEN("The value is read correctly") {
-      REQUIRE( value.compare("foo_ident") == 0 );
-    }
-    AND_THEN("The next character is the : delimiter") {
-      REQUIRE( (char)input.get() == ',' );
-    }
-  }
-  WHEN("ParseValue is called with a } delimiter") {
-    std::istringstream input("   foo_ident  }  ");
-    std::string value = Parser::parseValue(input);
-    THEN("The value is read correctly") {
-      REQUIRE( value.compare("foo_ident") == 0 );
-    }
-    AND_THEN("The next character is the : delimiter") {
-      REQUIRE( (char)input.get() == '}' );
-    }
-  }
-  WHEN("ParseValue is called with a no delimiter") {
-    std::istringstream input("   foo_ident    ");
-    std::string value = Parser::parseValue(input);
-    THEN("The value is read correctly") {
-      REQUIRE( value.compare("foo_ident") == 0 );
-    }
-    AND_THEN("The stream eof bit is set") {
-      REQUIRE( input.eof() );
-    }
-  }
-  WHEN("parseValue is called on a string with quotes") {
-    std::istringstream input("  \"woop, spaces and }:{ \" ");
-    std::string value = Parser::parseValue(input);
-    THEN("Spaces and delimiters make it into the string") {
-      CAPTURE( value );
-      REQUIRE( value.compare("woop, spaces and }:{ ")==0 );
-    }
-  }
-}
+/// Test whether ParsePair works as expected
+SCENARIO("Check whether ParsePair works independently of ParseDict") {
+  Parser parser;
 
-SCENARIO("ParsePair works independantly") {
-  WHEN("parsePair is called on a string : string pair") {
-    std::istringstream input(" foo : bar ");
-    std::pair<std::string, Definition*> pair = Parser::parsePair(input);
-    THEN("The pair is correctly extracted") {
-      CAPTURE( pair.first );
-      CAPTURE( pair.second->value );
-      REQUIRE( pair.first.compare("foo") == 0 );
-      REQUIRE( pair.second->value.compare("bar") == 0 );
-    }
-  }
-  WHEN("parsePair is called on a string : dict pair") {
-    std::istringstream input(" foo : { nest1 : bar }");
-    std::pair<std::string, Definition*> pair = Parser::parsePair(input);
-    THEN("The pair is correctly extracted") {
-      CAPTURE( pair.first );
-      REQUIRE( pair.first.compare("foo") == 0 );
-      REQUIRE( pair.second->pairs["nest1"]->value.compare("bar") == 0 );
-    }
-  }
-}
+  std::vector<Token> tokens;
+  std::vector<ParserError> errors;
+  std::pair<std::string, Definition*> pair;
+  std::vector<Token>::iterator tokenItr;
 
-SCENARIO("parseDict works independantly") {
-  WHEN("parseDict is called on a flat dict") {
-    std::istringstream input(" foo:bar, who:wat }");
-    Definition * def = Parser::parseDict(input);
-    THEN("The dictionary is correctly extracted") {
-      REQUIRE( def->pairs["foo"]->value.compare("bar") == 0 );
-      REQUIRE( def->pairs["who"]->value.compare("wat") == 0 );
+  // Test whether ParsePair works as expected when the pair does not
+  // contain a dict
+  GIVEN("Pair does not contain a dict") {
+    GIVEN("Pair starts with an identifier") {
+      tokens.push_back(Token(TokenType::IDENTIFIER, "identifier1", 1, 1));
+      tokens.push_back(Token(TokenType::PAIRSEPARATOR, ":", 1, 12));
+
+      WHEN("Second part is an identifier") {
+        tokens.push_back(Token(TokenType::IDENTIFIER, "identifier2", 1, 13));
+
+        tokenItr = tokens.begin();
+
+        THEN("ParsePair should return this pair successfully") {
+          pair = parser.parsePair(tokens, tokenItr, errors);
+
+          CHECK(pair.first == "identifier1");
+          REQUIRE(pair.second != 0);
+          CHECK(pair.second->value == "identifier2");
+          CHECK(pair.second->pairs.empty());
+          CHECK(pair.second->type == FieldType::Value);
+        }
+      }
+
+      WHEN("Second part is a value") {
+        tokens.push_back(Token(TokenType::IDENTIFIER, "va.lue", 1, 13));
+
+        tokenItr = tokens.begin();
+
+        THEN("ParsePair should return this pair successfully") {
+          pair = parser.parsePair(tokens, tokenItr, errors);
+
+          CHECK(pair.first == "identifier1");
+          REQUIRE(pair.second != 0);
+          CHECK(pair.second->value == "va.lue");
+          CHECK(pair.second->pairs.empty());
+          CHECK(pair.second->type == FieldType::Value);
+        }
+      }
+
+      WHEN("Second part is a string") {
+        tokens.push_back(Token(TokenType::IDENTIFIER, "a string", 1, 13));
+
+        tokenItr = tokens.begin();
+
+        THEN("ParsePair should return this pair successfully") {
+          pair = parser.parsePair(tokens, tokenItr, errors);
+
+          CHECK(pair.first == "identifier1");
+          REQUIRE(pair.second != 0);
+          CHECK(pair.second->value == "a string");
+          CHECK(pair.second->pairs.empty());
+          CHECK(pair.second->type == FieldType::Value);
+        }
+      }
     }
-  }
-}
 
-SCENARIO("parseDefinition works on a correctly formatted file") {
-  WHEN("parseDefinition is called on test1.def") {
-    Definition * def = Parser::parseDefinition("tests/parser/test_files/test1.def");
+    GIVEN("Pair starts with a string") {
+      tokens.push_back(Token(TokenType::STRING, "a string 1", 1, 1));
+      tokens.push_back(Token(TokenType::PAIRSEPARATOR, ":", 1, 11));
 
-    THEN("All the values are present and correct") {
-      REQUIRE( def->pairs["foo"]->value.compare("foo bar") == 0 );
-      REQUIRE( def->pairs["key"]->pairs["subkey"]->value.compare("sub_value") == 0 );
-      REQUIRE( def->pairs["key"]->pairs["subkey2"]->value.compare("spaced sub value") == 0 );
-      REQUIRE( def->pairs["key"]->pairs["lorem"]->value.compare("ipsum") == 0 );
-      REQUIRE( def->pairs["pig"]->value.compare("latin") == 0 );
+      tokenItr = tokens.begin();
+
+      WHEN("Second part is an identifier") {
+        tokens.push_back(Token(TokenType::IDENTIFIER, "identifier2", 1, 12));
+
+        tokenItr = tokens.begin();
+
+        THEN("ParsePair should return this pair successfully") {
+          pair = parser.parsePair(tokens, tokenItr, errors);
+
+          CHECK(pair.first == "a string 1");
+          REQUIRE(pair.second != 0);
+          CHECK(pair.second->value == "identifier2");
+          CHECK(pair.second->pairs.empty());
+          CHECK(pair.second->type == FieldType::Value);
+        }
+      }
+
+      WHEN("Second part is a value") {
+        tokens.push_back(Token(TokenType::IDENTIFIER, "va.lue", 1, 12));
+
+        tokenItr = tokens.begin();
+
+        THEN("ParsePair should return this pair successfully") {
+          pair = parser.parsePair(tokens, tokenItr, errors);
+
+          CHECK(pair.first == "a string 1");
+          REQUIRE(pair.second != 0);
+          CHECK(pair.second->value == "va.lue");
+          CHECK(pair.second->pairs.empty());
+          CHECK(pair.second->type == FieldType::Value);
+        }
+      }
+
+      WHEN("Second part is a string") {
+        tokens.push_back(Token(TokenType::IDENTIFIER, "a string 2", 1, 12));
+
+        tokenItr = tokens.begin();
+
+        THEN("ParsePair should return this pair successfully") {
+          pair = parser.parsePair(tokens, tokenItr, errors);
+
+          CHECK(pair.first == "a string 1");
+          REQUIRE(pair.second != 0);
+          CHECK(pair.second->value == "a string 2");
+          CHECK(pair.second->pairs.empty());
+          CHECK(pair.second->type == FieldType::Value);
+        }
+      }
     }
   }
 }

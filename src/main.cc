eed5081faf2d9428274b526ed6e158fc62bdcf5f
Duncan Barber <dab67@cam.ac.uk>
2015-06-03 10:49:47 +0100

    Committing main file which provides some detail about the output from the lexor and parser - used for testing

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,56 @@
-#include "gui/gui.h"
+#include <iostream>
 
-wxIMPLEMENT_APP(MyApp);
+#include "parser/parser.h"
+
+using namespace std;
+
+//This is a dummy main, feel free to overwrite it in any merge
+int main(void) {
+
+  string filename("tests/parser/test_files/test1.def");
+
+  Parser parser;
+  Lexor lexor;
+
+  vector<ParserError> errors;
+  vector<Token> tokens;
+
+  lexor.lex(filename, errors, tokens);
+
+  printf("LEXING OUTPUT:\n\n");
+
+  for (vector<Token>::iterator itr = tokens.begin(); itr != tokens.end(); ++itr) {
+    printf( "Line: %u, Char: %u, Token: %s\n",
+            (int)itr->getStartLineNo(),
+            (int)itr->getStartCharNo(),
+            itr->getValue().c_str());
+  }
+  for (vector<ParserError>::iterator itr = errors.begin(); itr != errors.end(); ++itr) {
+    printf("Line: %u, Char: %u, Error: %s\n",
+      (int)itr->getLineNo(),
+      (int)itr->getCharNo(),
+      itr->getErrorDescription().c_str());
+  }
+
+  errors.clear();
+
+  printf("\n\n\n");
+
+  Definition* def = parser.parse(filename, errors);
+
+  printf("PARSING OUTPUT:\n\n");
+
+  for (vector<ParserError>::iterator itr = errors.begin(); itr != errors.end(); ++itr) {
+    printf("Line: %u, Char: %u, Error: %s\n",
+      (int)itr->getLineNo(),
+      (int)itr->getCharNo(),
+      itr->getErrorDescription().c_str());
+  }
+  def->print();
+
+  errors.clear();
+
+  printf("\n\n\n");
+
+  return 0;
+}
